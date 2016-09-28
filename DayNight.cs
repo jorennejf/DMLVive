03263a9eb3f7add1e4fb80476b89d5166e45bb40
jorennejf <jorennejf@gmail.com>
2016-09-28 16:52:50 -0700

    Added files via upload
    DayNight working correctly on Vive headset; menuScript not compatible with Vive

---
+++ DayNight.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System;
+
+
+/*
+ * Description: Uses the current hour (in 24h) to determine whether to display
+ * 				day or night sky.
+ * 
+ */
+
+public class DayNight : MonoBehaviour {
+
+
+	public GameObject clouds;
+	//public GameObject stars;
+	SkyboxSwitcher stars; //script that contains the night sky
+	public int sunriseHour = 6; //static?
+	public int sunsetHour = 18; //static?
+	//public Material daySky; 
+	//public Material nightSky;
+
+	// Initializes the animations according to system time
+	void Start () {
+		var time = System.DateTime.Now.Hour;
+		//Debug.Log (System.DateTime.Now.Hour.ToString ()); //prints to console the current hour in 24h
+		stars = GetComponent<SkyboxSwitcher>();
+
+		//if nighttime
+		if (time >= sunsetHour || time <= sunriseHour) {
+			makeNight();
+			//RenderSettings.skybox = nightSky;
+		}
+
+		//if daytime
+		else{
+			makeDay();
+			//RenderSettings.skybox = daySky;
+		}
+
+	}
+
+	private void makeDay(){
+		clouds.SetActive (true);
+		stars.enabled = false;
+	}
+
+	private void makeNight(){
+		clouds.SetActive (false);
+		stars.enabled = true;
+	}
+	
+
+}
---
+++ menuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class menuScript : MonoBehaviour {
+
+
+	public Canvas quitMenu;
+	public Button quitButton;
+	
+
+
+	// Use this for initialization
+
+	void Start () {
+
+		quitMenu = quitMenu.GetComponent<Canvas>();
+		quitButton = quitButton.GetComponent<Button>();
+		quitMenu.enabled = false;
+	
+}
+	
+	
+
+	//When "Quit" button is pressed
+	public void QuitPress(){
+		quitMenu.enabled = true;
+		quitButton.enabled = false;
+	}
+
+	//When "No" is pressed on Quit Menu
+	public void NoPress(){
+		quitMenu.enabled = false;
+		quitButton.enabled = true;
+	}
+
+	//When "Yes" is pressed on Quit Menu
+	public void YesPress(){
+		Application.Quit();
+	}
+
+}
